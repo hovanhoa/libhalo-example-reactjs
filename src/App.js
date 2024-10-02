588c555a71a03c7bfa1cdff78aee90ef56b0a8f4
hovanhoa <hovanhoaa1@gmail.com>
2024-10-02 14:11:05 +0700

    a

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {execHaloCmdWeb} from "@arx-research/libhalo/api/web.js";
 
 function App() {
-    const [statusText, setStatusText] = useState('Click on the button 2');
+    const [statusText, setStatusText] = useState('Click on the button 3');
 
     async function btnClick() {
         let command = {
