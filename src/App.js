36d2901377c761255e1cf2551708dbadadd79444
hovanhoa <hovanhoaa1@gmail.com>
2024-10-03 11:34:52 +0700

    feat: update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {execHaloCmdWeb} from "@arx-research/libhalo/api/web.js";
 
 function App() {
-    const [statusText, setStatusText] = useState('Click on the button 6');
+    const [statusText, setStatusText] = useState('Click on the button 7');
 
     async function btnClick() {
         // let command = {
